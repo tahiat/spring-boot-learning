667a599453431128f1ad69e137dde5aa5647521c
tahiat <tahiatul@gmail.com>
2020-04-11 15:25:28 +0600

    adding all files in git

---
+++ src/main/java/com/example/spring_starter/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.example.spring_starter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
